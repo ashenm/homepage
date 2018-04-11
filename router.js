6a16eed63397847aa2d74bba2d67d97fa46a4159
Ashen Gunaratne <mail@ashenm.ml>
2018-04-11 17:25:31 +0530

    Route changes to reflect standalone resume and minor improvements

--- router.js
+++ router.js
@@ -16,26 +16,21 @@ const fs = require('fs');
 // custom route handler
 const delegate = new loader(path.join(__dirname, '.data', 'routes.txt'));
 
-// parse resume data
-const resume = JSON.parse(fs.readFileSync(path.join(__dirname, '.data', 'resume.json'), 'utf8'));
-
 // webroot
 router.get('/', (request, response) => response.render('index'));
 
 // resume
 router.get('/resume', (request, response) => {
-  if (!request.query.dl) {
-    response.render('resume', {resume: resume});
-  } else {
-    response.set('Content-Disposition', 'attachment; filename="resume-ashenm.pdf"');
-    response.sendFile(path.join(__dirname, '.cache', 'resume.pdf'));
-  }
+  !request.query.dl
+    ? response.sendFile(path.join(__dirname, '.data', 'resume.html'))
+    : response.download(path.join(__dirname, '.cache', 'resume.pdf'), 'resume-ashenm.pdf');
 });
 
 // redirect matching custom routes
 router.use((request, response, next) => {
-  const target = delegate.route(request.path);
-  target ? response.redirect(301, target) : next();
+  const target = delegate.route(request.path)
+    ? response.redirect(301, target)
+    : next();
 });
 
 // resourse not found
