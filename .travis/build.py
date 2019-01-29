bbb39dd9fb23adca722954a67a3e7b198b2a18fc
Ashen Gunaratne <hosting@ashenm.ml>
2019-01-29 15:34:26 +0000

    [Skip CI] Tidy
    - Remove Travis log folds

--- .travis/build.py
+++ .travis/build.py
@@ -3,11 +3,7 @@
 import os
 
 # build custom routes
-print('travis_fold:start:build_routes\033[33;1mbuild routes\033[0m')
 os.spawnlp(os.P_WAIT, 'python3', 'python', '.travis/routes.py')
-print('\ntravis_fold:end:build_routes\r')
 
 # build site
-print('travis_fold:start:build_site\033[33;1mbuild site\033[0m')
 os.spawnlp(os.P_WAIT, 'jekyll', 'jekyll', 'build')
-print('\ntravis_fold:end:build_site\r')
